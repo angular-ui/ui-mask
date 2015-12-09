0d5cc7e3c3d98bd8207c974aac4acc9c9bc05c4f
Oleksandr Khomenko <okhomenko@gmail.com>
2015-12-09 14:34:00 -0600

    Moved index outside of ./{src,dist} so it's not gonna be processed with gulp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //https://github.com/angular/angular.js/pull/10732
 
 var angular = require('angular');
-var mask = require('./mask');
+var mask = require('./dist/mask');
 
 module.exports = 'ui.mask';
